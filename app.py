75b08a28e957e9745627255f50bd51cfe9075ee1
HypeGawd <91508281+HypeGawd@users.noreply.github.com>
2023-03-19 21:20:32 -0400

    Got the functionality done for feature #3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, request, redirect, render_template
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -7,6 +7,12 @@ app = Flask(__name__)
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
 
+movie1 = movie_repository.create_movie("Avengers", "A Director", 5)
+movie_repository._db[0] = movie1
+movie2 = movie_repository.create_movie("Cars", "Some Director", 3)
+movie_repository._db[1] = movie2
+movie3 = movie_repository.create_movie("Cars", "The Director", 4)
+movie_repository._db[2] = movie3
 
 @app.get('/')
 def index():
@@ -31,10 +37,12 @@ def create_movie():
     return redirect('/movies')
 
 
-@app.get('/movies/search')
+@app.get('/movies/search/')
 def search_movies():
-    # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
+        title = request.args.get('title')
+        movies = movie_repository.get_movie_by_title(title)
+        #movie_title = movies.title
+        return render_template('search_movies.html', movies = movies, search_active=True)
 
 
 @app.get('/movies/<int:movie_id>')
