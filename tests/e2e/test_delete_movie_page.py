221cfe004203d1cafc68a38c56ccde75c2c6f554
AnuragDasari <anuragchowdary@outlook.com>
2023-03-22 17:18:20 -0400

    Tests added

--- tests/e2e/test_delete_movie_page.py
+++ tests/e2e/test_delete_movie_page.py
@@ -1,1 +1,21 @@
 # TODO: Feature 6
+from app import app
+from src.repositories.movie_repository import get_movie_repository
+
+test_app = app.test_client()
+
+def test_delete_movie():
+
+    # Create a temporary movie to delete
+    movie_repository = get_movie_repository()
+    temp_movie = movie_repository.create_movie('Avengers Endgame', 'Russo Brothers', 5)
+
+    # Send a request to delete the movie
+    response = test_app.post(f'/movies/{temp_movie.movie_id}/delete')
+
+    # Check that the response is a redirect to the list all movies page
+    assert response.status_code == 302 
+
+    # Check that the movie was deleted from the database
+    deleted_movie = movie_repository.get_movie_by_id(temp_movie.movie_id)
+    assert deleted_movie is None
--- tests/unit/test_delete_movie.py
+++ tests/unit/test_delete_movie.py
@@ -1,15 +1,16 @@
 # TODO: Feature 6
-from tests.e2e.conftest import test_app
 from src.repositories.movie_repository import get_movie_repository
 
-def test_delete_movie(test_app):
+def test_delete_movie():
     movie_repository = get_movie_repository()
+
+    # Create a temporary movie to delete
     temp_movie = movie_repository.create_movie('Avengers Endgame', 'Russo Brothers', 5)
     movie_id = temp_movie.movie_id
-    response = test_app.post(f'/movies/{movie_id}/delete')
+    
+    # Delete movie
+    movie_repository.delete_movie(movie_id)
 
-    # Check that the response is a redirect to the list all movies page
-    assert response.status_code == 302 
-    # Check that the movie was deleted from the database
+    # Check that the movie was deleted
     deleted_movie = movie_repository.get_movie_by_id(movie_id)
     assert deleted_movie is None
