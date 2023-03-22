9b6daef8c6fe57f74b3462c3af0250b7424338b5
AnuragDasari <anuragchowdary@outlook.com>
2023-03-22 17:50:01 -0400

    Tests updated

--- tests/e2e/test_delete_movie_page.py
+++ tests/e2e/test_delete_movie_page.py
@@ -8,14 +8,29 @@ def test_delete_movie():
 
     # Create a temporary movie to delete
     movie_repository = get_movie_repository()
-    temp_movie = movie_repository.create_movie('Avengers Endgame', 'Russo Brothers', 5)
+    # Clear database
+    movie_repository.clear_db()
+    temp_movie1 = movie_repository.create_movie('Avengers Endgame', 'Russo Brothers', 5)
+    temp_movie2 = movie_repository.create_movie('Avengers Infinity war', 'Russo Brothers', 4)
+    temp_movie3 = movie_repository.create_movie('The Avengers', 'Joss Whedon', 4)
+    movie_repository._db[0] = temp_movie1
+    movie_repository._db[1] = temp_movie2
+    movie_repository._db[2] = temp_movie3
 
     # Send a request to delete the movie
-    response = test_app.post(f'/movies/{temp_movie.movie_id}/delete')
+    response = test_app.post(f'/movies/{temp_movie1.movie_id}/delete')
+    response = test_app.post(f'/movies/{temp_movie2.movie_id}/delete')
 
     # Check that the response is a redirect to the list all movies page
     assert response.status_code == 302 
 
     # Check that the movie was deleted from the database
-    deleted_movie = movie_repository.get_movie_by_id(temp_movie.movie_id)
-    assert deleted_movie is None
+    deleted_movie1 = movie_repository.get_movie_by_id(temp_movie1.movie_id)
+    deleted_movie2 = movie_repository.get_movie_by_id(temp_movie2.movie_id)
+
+    # Check that the movie was not deleted from the dateabase
+    deleted_movie3 = movie_repository.get_movie_by_id(temp_movie3.movie_id)
+
+    assert deleted_movie1 is None
+    assert deleted_movie2 is None
+    assert deleted_movie3 is not None
--- tests/unit/test_delete_movie.py
+++ tests/unit/test_delete_movie.py
@@ -5,12 +5,26 @@ def test_delete_movie():
     movie_repository = get_movie_repository()
 
     # Create a temporary movie to delete
-    temp_movie = movie_repository.create_movie('Avengers Endgame', 'Russo Brothers', 5)
-    movie_id = temp_movie.movie_id
-    
+    temp_movie1 = movie_repository.create_movie('Avengers Endgame', 'Russo Brothers', 5)
+    temp_movie2 = movie_repository.create_movie('Avengers Infinity war', 'Russo Brothers', 4)
+    temp_movie3 = movie_repository.create_movie('The Avengers', 'Joss Whedon', 4)
+
+    movie_id1 = temp_movie1.movie_id
+    movie_id2 = temp_movie2.movie_id
+    movie_id3 = temp_movie3.movie_id
+
     # Delete movie
-    movie_repository.delete_movie(movie_id)
+    movie_repository.delete_movie(movie_id1)
+    movie_repository.delete_movie(movie_id2)
 
     # Check that the movie was deleted
-    deleted_movie = movie_repository.get_movie_by_id(movie_id)
-    assert deleted_movie is None
+    deleted_movie1 = movie_repository.get_movie_by_id(movie_id1)
+    assert deleted_movie1 is None
+
+    # Check that the movie was deleted
+    deleted_movie2 = movie_repository.get_movie_by_id(movie_id2)
+    assert deleted_movie2 is None
+
+    # Check that the movie was not deleted
+    deleted_movie3 = movie_repository.get_movie_by_id(movie_id3)
+    assert deleted_movie3 is not None
