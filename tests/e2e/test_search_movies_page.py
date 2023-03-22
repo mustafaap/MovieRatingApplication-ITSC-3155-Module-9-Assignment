cf0d630d3316fa8d10ac718139a671a2d68ecdca
HypeGawd <91508281+HypeGawd@users.noreply.github.com>
2023-03-22 16:48:47 -0400

    added tests for search_movies function

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,1 +1,29 @@
 # TODO: Feature 3
+from tests.e2e.conftest import test_app
+from src.repositories.movie_repository import get_movie_repository
+
+def test_search_movies_page(test_app):
+    movie_rep = get_movie_repository()
+
+    # Clear database
+    movie_rep.clear_db()
+
+    # Create some movies to search for
+    movie1 = movie_rep.create_movie('The Shawshank Redemption', 'Frank Darabont', 5)
+    movie2 = movie_rep.create_movie('The Godfather', 'Francis Ford Coppola', 5)
+    movie3 = movie_rep.create_movie('The Dark Knight', 'Christopher Nolan', 4)
+
+    # Test searching for existing movie
+    response = test_app.get('/movies/search?title=The Shawshank Redemption')
+    assert response.status_code == 200
+    assert b'<td>The Shawshank Redemption</td>' in response.data
+    assert b'<td>Frank Darabont</td>' in response.data
+    assert b'<td>5</td>' in response.data
+
+    # Test searching for non-existent movie
+    response = test_app.get('/movies/search?title=Interstellar')
+    assert response.status_code == 200
+    assert b'<p>No results found for "Interstellar".</p>' in response.data
+
+    # Clear database
+    movie_rep.clear_db()
--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,26 @@
 # TODO: Feature 3
+from tests.e2e.conftest import test_app
+from src.repositories.movie_repository import get_movie_repository
+
+def test_search_movies(test_app):
+    movie_rep = get_movie_repository()
+
+    # Clear database
+    movie_rep.clear_db()
+
+    # Adds movies to database
+    temp_movie1 = movie_rep.create_movie('Avengers', 'Russo Brothers', 5)
+    temp_movie2 = movie_rep.create_movie('Spider-Man', 'Jon Watts', 3)
+
+    # Search for the movie by title
+    response = test_app.get('/movies/search?title=Avengers')
+
+    # Check if the response has the correct status code
+    assert response.status_code == 200
+
+    # Check if the returned HTML contains the movie's title and director
+    assert temp_movie1.title in response.data.decode('utf-8')
+    assert temp_movie1.director in response.data.decode('utf-8')
+
+    # Clear database
+    movie_rep.clear_db()
